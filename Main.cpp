60bd729d9553bf9617382e5a9cfa3a77fd0be1a6
DragonCrymlll <72498300+DragonCrymlll@users.noreply.github.com>
2020-10-22 20:02:45 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -13,41 +13,102 @@ int MENU(){
     return pilih;
 }
 
-void Gauss(float *x){
-
-}
-
-void GaussJordan(float *x){
-
-}
-
-void Invers(float *x){
-
-}
-
-void Crammer(float *x){
-
-}
-
 void SPL(){
-    int n,i,j;
-    cin>>n;
-    float a[n][n];
+    int n,m;
+    
+    cout<<"Banyaknya SPL : ";cin>>n;
+    cout<<"Banyak variabel SPL : ";cin>>m;
+    float a[n][m+1];
 
-    for(i=0;i<=n;i++){
-        for(j=0;j<n;j++){
+    //input
+    cout<<"Input SPL : "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=m;j++){
             cin>>a[i][j];
-
         }
     }
 
     int pilih = MENU();
-    switch(pilih){
-        case 1 : Gauss(&a[i][j]);break;
-        case 2 : GaussJordan(&a[i][j]);break;
-        case 3 : Invers(&a[i][j]);break;
-        case 4 : Crammer(&a[i][j]);break;
-        default :break;
+
+    if(pilih==1){
+        float hasil[m];
+        //menu gauss
+        int o=0;
+        int p=1;
+        
+        do{
+            //pembagian agar menjadi 1 untuk semua kolom
+            for(int i=0+o;i<n;i++){
+                float temp= a[i][o];
+                for(int j=0+o;j<=m;j++){
+                    a[i][j]= float(a[i][j]/temp);
+                }
+            }
+            //pengurangan agar menjadi 0 untuk setiap kolom
+            for(int i=0+p;i<n;i++){
+                for(int j=0+o;j<=m;j++){
+                    a[i][j]=float(a[i][j]-a[o][j]);
+                }
+            }
+            o++;p++;
+        }while(o<=m);
+
+        //hasil tampil gauss
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=m;j++){
+                cout<<a[i][j]<<" ";
+            }cout<<endl;
+        }cout<<endl;
+        
+        //penjumlahan
+        o=m;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=m;j++){
+            }cout<<endl;
+        }
+        //hasil variabel spl
+        for(int i=1;i<=m;i++){
+            cout<<"X"<<i<<" : "<<hasil[i]<<endl;
+        }
+    }
+
+    else if(pilih==2){
+        //menu gaussjordan
+        float hasil[m];
+        //menu gauss
+        int o=0;
+        int p=1;
+
+        do{
+            //pembagian agar menjadi 1 untuk semua kolom
+            for(int i=0+o;i<n;i++){
+                float temp= a[i][o];
+                for(int j=0+o;j<=m;j++){
+                    a[i][j]= float(a[i][j]/temp);
+                }
+            }
+            //pengurangan agar menjadi 0 untuk setiap kolom
+            for(int i=0+p;i<n;i++){
+                for(int j=0+o;j<=m;j++){
+                    a[i][j]=float(a[i][j]-a[o][j]);
+                }
+            }
+            o++;p++;
+        }while(o<=m);
+        
+        //hasil gaussjordan
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=m;j++){
+                cout<<a[i][j]<<" ";
+            }cout<<endl;
+        }cout<<endl;
+    }
+    else if(pilih==3){
+        //menu invers
+    }else if(pilih==4){
+        //menu crammer
+    }else{
+        cout<<"Pilihan tidak tersedia!"<<endl;
     }
 }
 
@@ -59,7 +120,6 @@ void MatriksBalikan(){
     
 }
 
-
 int main(){
     int n;
     int pilih;
@@ -69,9 +129,9 @@ int main(){
     cout<<"2. Determinan"<<endl;
     cout<<"3. Matriks Balikan"<<endl;
     cout<<"4. Keluar"<<endl;
-    cin>>pilih;
+    cout<<"=================================="<<endl;
+    cout<<"Pilih : ";cin>>pilih;
     
-
     switch(pilih){
         case 1 : SPL();break;
         case 2 : DET();break;
@@ -81,3 +141,9 @@ int main(){
     }
 
 }
+//penjumlahan
+        /*for(int i=n-1;i>=0;i--){
+            for(int j=m;j>=0;j--){
+                cout<<a[i][j]<<" ";
+            }cout<<endl;
+        }cout<<endl;*/
